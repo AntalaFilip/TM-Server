dbb3304109b32abd89d086453a584904580ca1fa
Filip Antala <filip@ahst.sk>
2022-06-26 09:54:38 +0200

    added sio admin ui

--- src/helpers/sio.ts
+++ src/helpers/sio.ts
@@ -1,5 +1,9 @@
 import { Server as SIOServer } from "socket.io";
-import { Server as HTTPServer } from 'http'
+import { Server as HTTPServer } from 'http';
+import { instrument } from '@socket.io/admin-ui';
+import bcrypt from 'bcrypt';
+
+const servers: Array<SIOServer> = [];
 
 function createSIOServer(http: HTTPServer): SIOServer {
 	const io = new SIOServer(http, {
@@ -10,8 +14,16 @@ function createSIOServer(http: HTTPServer): SIOServer {
 			credentials: true,
 		},
 	});
+	instrument(io, {
+		auth: {
+			username: 'administrator',
+			password: bcrypt.hashSync(process.env.SIO_ADMIN_PWD ?? 'admin123', 10),
+			type: 'basic',
+		}
+	});
 
+	servers.push(io);
 	return io;
 };
 
-export { createSIOServer };
+export { createSIOServer, servers };
