97b1fce4935a761bee3ca945fe3be403080c83c7
Filip Antala <filip@ahst.sk>
2022-06-30 16:35:16 +0200

    fix: error on undefined realm, add check for global admin

--- src/types/user.ts
+++ src/types/user.ts
@@ -114,8 +114,9 @@ class User extends Resource {
 	hasPermission(perm: Permission, realm?: Realm): boolean {
 		return (
 			(this.permissions.global & PermissionMap[perm]) === PermissionMap[perm]
-			|| (this.permissions.realm.get(realm.id) & PermissionMap[perm]) === PermissionMap[perm]
+			|| (this.permissions.realm.get(realm?.id) & PermissionMap[perm]) === PermissionMap[perm]
 			|| realm?.owner === this
+			|| this.admin
 		);
 	}
 
