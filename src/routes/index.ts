1c5969477018397098cebc53967f152969ba5049
Filip Antala <filip@ahst.sk>
2022-06-30 18:48:08 +0200

    added use of cookieParser

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import Express from 'express';
 import Client from '../types/client';
 import createAuthRouter from './auth';
 import createRealmsRouter from './realms';
+import cookieParser from 'cookie-parser';
 
 function createIndexRouter(client: Client) {
 	const router = Express.Router();
+	router.use(cookieParser());
 
 	const authRouter = createAuthRouter(client);
 	// const realmsRouter = createRealmsRouter(client);
