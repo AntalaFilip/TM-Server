4f29f89f0e6779bb6f06af2af8fb1872f26006cb
Filip Antala <filip@ahst.sk>
2022-07-01 16:33:28 +0200

    added track creation route

--- src/routes/realms/station.ts
+++ src/routes/realms/station.ts
@@ -1,6 +1,6 @@
 import { json, Response, Router } from "express";
 import { authenticate } from "../../middleware/httpauth";
-import { TMRealmRequest } from "../../middleware/realmParser";
+import realmParser, { TMRealmRequest } from "../../middleware/realmParser";
 import stationParser, { TMStationRequest } from "../../middleware/stationParser";
 import trackParser, { TMTrackRequest } from "../../middleware/trackParser";
 import Client from "../../types/client";
@@ -23,7 +23,7 @@ function createStationRouter(client: Client) {
 
 	stationSpecificRouter.use('/tracks', trackRouter);
 	trackRouter.get('/', getAllTracks);
-	// trackRouter.post('/');
+	trackRouter.post('/', createTrack);
 
 	trackRouter.use('/:track', trackParser.bind(undefined, true), trackSpecificRouter);
 	trackSpecificRouter.get('/', getTrack);
@@ -48,6 +48,19 @@ function updateTrack(req: TMTrackRequest, res: Response) {
 	const mdf = track.modify(toUpdate, user);
 	if (mdf) return res.status(200).send(track.metadata());
 	else return res.status(400).send({ message: `Invalid data`, error: { code: `EBADREQUEST` } });
+}
+
+async function createTrack(req: TMStationRequest, res: Response) {
+	const { station, auth: user, realm } = req;
+	if (!user.hasPermission('manage stations')) return res.status(403).send({ message: `No permission`, error: { code: `ENOPERM` } });
+
+	const data = req.body;
+	if (typeof data.name != 'string') return res.status(400).send({ message: `Missing parameters!`, error: { code: `EBADREQUEST`, extension: { code: `CRTSTNTRK-EBADPARAM-NAME` } } });
+	if (data.length && typeof data.length != 'number') return res.status(400).send({ message: `Missing parameters!`, error: { code: `EBADREQUEST`, extension: { code: `CRTSTNTRK-EBADPARAM-LENGTH` } } });
+	if (typeof data.usedForParking != 'boolean') return res.status(400).send({ message: `Missing parameters!`, error: { code: `EBADREQUEST`, extension: { code: `CRTSTNTRK-EBADPARAM-USEDFORPARKING` } } });
+
+	const track = await station.addTrack({ realmId: realm.id, managerId: realm.stationManager.id, name: data.name, stationId: station.id, length: data.length, usedForParking: data.usedForParking });
+	res.status(201).send(track.metadata());
 }
 
 function getTrack(req: TMTrackRequest, res: Response) {
@@ -87,7 +100,7 @@ async function createStation(req: TMRealmRequest, res: Response) {
 
 
 	const station = await smgr.create({ realmId: realm.id, managerId: smgr.id, name: data.name, stationType: data.stationType }, user);
-	return res.status(201).send(station);
+	return res.status(201).send(station.metadata());
 }
 
 async function updateStation(req: TMStationRequest, res: Response) {
